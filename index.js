3da1513619f9412b9f21ec5fc48de12b32385774
kiet_dev <79886528+kietdev123@users.noreply.github.com>
2024-07-15 23:10:16 +0700

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,21 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/blogs', (req, res) => {
+    res.status(200).json({
+        message : 'Get blogs successfully',
+        data :[
+            {
+                'title' : '1',
+                'image' : '2',
+                'discrible' : '2',
+                'content' : '232',
+            }
+        ],
+    });
+  })
+  
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
